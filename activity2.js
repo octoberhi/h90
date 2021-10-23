5a8c63682807d4386d0df81247276946fdc3d860
octoberhi <89272971+octoberhi@users.noreply.github.com>
2021-10-22 17:53:44 -0700

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,10 @@
+
+function back(){
+window.location="activity_1.html";
+}
+
+function get_score(){
+
+score= localStorage.getItem("score");
+document.getElementById("update").innerHTML="score: "+score;
+}
